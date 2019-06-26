6af942f3ebee6d7f5164ac4b1afdb47b9ec1c83e
fcastagna <44572424+fcastagna@users.noreply.github.com>
2019-06-26 17:31:08 +0200

    Update convergence.py

--- convergence.py
+++ convergence.py
@@ -10,33 +10,41 @@ nburn = 2000
 nchains = 3
 with pm.Model() as model:
     m = pm.Uniform('m', lower=0., upper=10.)
-    likelihood = pm.Normal('y_obs', mu=m, sd=2., observed=obs)
-    step_1 = pm.Metropolis(S=np.array([.01]))
-    step_2 = pm.Metropolis(S=np.array([10]))
-    step_3 = pm.Metropolis(S=np.array([2.5]))
+    likelihood = pm.Normal('y_obs', mu=m, sigma=2., observed=obs)
+    step_0 = pm.Metropolis(S=np.array([.01]))
+    step_1 = pm.Metropolis(S=np.array([10]))
+    step_2 = pm.Metropolis(S=np.array([2.5]))
+    trace_0 = pm.sample(niter, step=step_0, cores=1, tune=0, chains=nchains, random_seed=123)
     trace_1 = pm.sample(niter, step=step_1, cores=1, tune=0, chains=nchains, random_seed=123)
     trace_2 = pm.sample(niter, step=step_2, cores=1, tune=0, chains=nchains, random_seed=123)
-    trace_3 = pm.sample(niter, step=step_3, cores=1, tune=0, chains=nchains, random_seed=123)
-
+    
+# save chains
+np.savetxt('chain_1.txt', trace_0.m)
+np.savetxt('chain_2.txt', trace_1.m)
+np.savetxt('chain_3.txt', trace_2.m)
+    
 # burn-in
+t_0 = trace_0[nburn:]
 t_1 = trace_1[nburn:]
 t_2 = trace_2[nburn:]
-t_3 = trace_3[nburn:]
 
 # traceplot
-pm.traceplot(trace_1[:])
-plt.axvline(nburn, color='r', linestyle=':'); plt.ylim(0, 7)
-pm.traceplot(trace_2[:])
-plt.axvline(nburn, color='r', linestyle=':'); plt.ylim(0, 7)
-pm.traceplot(trace_3[:])
-plt.axvline(nburn, color='r', linestyle=':'); plt.ylim(0, 7)
+pm.traceplot(trace_0, var_names=['m'], figsize=(16, 6), trace_kwargs={'linewidth': 2}, textsize=20)
+plt.axvline(nburn, color='r', linestyle=':')
+pm.traceplot(trace_1, var_names=['m'], figsize=(16, 6), trace_kwargs={'linewidth': 2}, textsize=20)
+plt.axvline(nburn, color='r', linestyle=':')
+pm.traceplot(trace_2, var_names=['m'], figsize=(16, 6), trace_kwargs={'linewidth': 2}, textsize=20)
+plt.axvline(nburn, color='r', linestyle=':')
 
-pm.traceplot(trace_1[nburn:nburn+100])
-pm.traceplot(trace_2[nburn:nburn+100])
-pm.traceplot(trace_3[nburn:nburn+100])
+pm.traceplot(trace_0[nburn:nburn+100], var_names=['m'], figsize=(16, 6), textsize=20, trace_kwargs={'linewidth': 2})
+pm.traceplot(trace_1[nburn:nburn+100], var_names=['m'], figsize=(16, 6), textsize=20, trace_kwargs={'linewidth': 2})
+pm.traceplot(trace_2[nburn:nburn+100], var_names=['m'], figsize=(16, 6), textsize=20, trace_kwargs={'linewidth': 2})
 
 # posterior distribution
-pm.plot_posterior(t_2, density=True)
+plt.rcParams['figure.figsize'] = (16, 12)
+plt.rcParams['xtick.labelsize'] = 30
+plt.rcParams['axes.labelsize'] = 40
+pm.plot_posterior(t_2, var_names=['m'], kind='hist', density=True)
 def prior(x):
     return st.uniform.pdf(x, 0., 10.) 
 def likelihood(x):
@@ -48,7 +56,7 @@ def posterior(x):
 x = np.arange(0., 10., .001)
 plt.plot(x, posterior(x), 'g')
 plt.xlim(0, 4)
-plt.legend(('94% HPD', 'analytical', 'simulated'))
+plt.legend(('94% HPD', 'analytical', 'simulated'), prop={'size': 40})
 
 # summary
 pm.summary(t_1)
@@ -56,38 +64,42 @@ pm.summary(t_2)
 pm.summary(t_3)
 
 # forest plot
-pm.forestplot([t_1, t_2, t_3])
+pm.forestplot([t_0, t_1, t_2], figsize=(16, 12), textsize=20, markersize=20)
 
 # acceptance rate
-print(step_1.accepted/(niter*nchains))
-print(step_2.accepted/(niter*nchains))
-print(step_3.accepted/(niter*nchains))
+print('Model 0: acc_rate = '+str(step_0.accepted/(niter*nchains)))
+print('Model 1: acc_rate = '+str(step_1.accepted/(niter*nchains)))
+print('Model 2: acc_rate = '+str(step_2.accepted/(niter*nchains)))
 
 # ACF
-pm.autocorrplot(t_1)
-pm.autocorrplot(t_2)
-pm.autocorrplot(t_3)
+pm.autocorrplot(t_0, var_names=['m'], combined=True, textsize=20)
+pm.autocorrplot(t_1, var_names=['m'], combined=True, textsize=20)
+pm.autocorrplot(t_2, var_names=['m'], combined=True, textsize=20)
 
 # ESS
+print(pm.effective_n(t_0))
 print(pm.effective_n(t_1))
 print(pm.effective_n(t_2))
-print(pm.effective_n(t_3))
 
 # Gelman-Rubin
+print(pm.gelman_rubin(t_0))
 print(pm.gelman_rubin(t_1))
 print(pm.gelman_rubin(t_2))
-print(pm.gelman_rubin(t_3))
 
 # Geweke
+plt.rcParams['figure.figsize'] = (16, 12)
+plt.rcParams['xtick.labelsize'] = 30
+plt.rcParams['ytick.labelsize'] = 30
+plt.rcParams['axes.labelsize'] = 40
+gew_0 = pm.geweke(t_0.m, first=.1, last=.5)
+plt.scatter(gew_0[:,0], gew_0[:,1])
 gew_1 = pm.geweke(t_1.m, first=.1, last=.5)
-plt.plot(gew_1[:,0], gew_1[:,1], '.')
+plt.scatter(gew_1[:,0], gew_1[:,1])
 gew_2 = pm.geweke(t_2.m, first=.1, last=.5)
-plt.plot(gew_2[:,0], gew_2[:,1], '.')
-gew_3 = pm.geweke(t_3.m, first=.1, last=.5)
-plt.plot(gew_3[:,0], gew_3[:,1], '.')
+plt.scatter(gew_2[:,0], gew_2[:,1])
 plt.xlabel('First iteration')
 plt.ylabel('Z-score')
 plt.ylim(-4, 4)
 plt.axhline(-st.norm.ppf(.95), linestyle='--')
-plt.axhline(st.norm.ppf(.95), linestyle='--')
-plt.legend(('Z-scores 1', 'Z-scores 2', 'Z-scores 3', '90% CI'), bbox_to_anchor=(1,.65))
+plt.axhline(st.norm.ppf(.95), linestyle='--', label='_nolegend_')
+plt.legend(('90% CI', 'Z-scores 0', 'Z-scores 1', 'Z-scores 2'), bbox_to_anchor=(1,1), prop={'size': 25})
